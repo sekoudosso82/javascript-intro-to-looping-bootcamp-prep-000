aa25605e41e96ca465a1a4a5defa70edbd2591db
Sekou Dosso <sekou.dosso82@gmail.com>
2019-12-31 16:28:54 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -26,3 +26,12 @@ function incrementVariable() {
   i = i + 1;
   return i;
 }
+
+function doWhileLoop(num)
+{
+  do {
+        console.log("I run once regardless.")
+       
+     } while (incrementVariable()< num+1)
+}
+         
